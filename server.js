015d597f5ee5b00731a1552f984454dbb2ece7d9
Negro31 <143755318+Negro31@users.noreply.github.com>
2025-08-29 02:04:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ let items = [];
 let gameStarted = false;
 let countdown = 10;
 let countdownInterval;
-let speedMultiplier = 1; // zamanla artacak
+let speedMultiplier = 3; // zamanla artacak
 
 // Oyuncu hareketi
 function movePlayers() {
@@ -60,8 +60,8 @@ io.on("connection", (socket) => {
       name: name,
       x: Math.random() * 550,
       y: Math.random() * 350,
-      vx: (Math.random() * 2 - 1) * 3, // daha hızlı
-      vy: (Math.random() * 2 - 1) * 3,
+      vx: (Math.random() * 2 - 1) * 10, // daha hızlı
+      vy: (Math.random() * 2 - 1) * 10,
       color: "#" + Math.floor(Math.random() * 16777215).toString(16),
       hp: 3,
       hasSpike: false,
