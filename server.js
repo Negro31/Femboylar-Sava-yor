28baa2d14457075c95ac998317b889a6ddc9a225
Negro31 <143755318+Negro31@users.noreply.github.com>
2025-08-28 22:47:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(__dirname));
+// Public klasörünü static dosya olarak ayarlıyoruz
+app.use(express.static(path.join(__dirname, "public")));
 
 let players = {};
 let items = [];
